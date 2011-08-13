d15abf7c3923f40cadb1caf232680c603ad15e1f
Ivan Denisov <d.ivan.krsk@gmail.com>
2011-08-14 04:31:53 +0800

    Подправил скрипт

--- setup_script.py
+++ setup_script.py
@@ -15,15 +15,13 @@ print >> OUT, "mv "+cwd+"/Lin "+cwd+"/hide/"
 print >> OUT, "mv "+cwd+"/System "+cwd+"/hide/"
 print >> OUT, "mv "+cwd+"/Host "+cwd+"/hide/"
 print >> OUT, "mv "+cwd+"/Code "+cwd+"/hide/"
+print >> OUT, "mv "+cwd+"/Sym "+cwd+"/hide/"
 
 print >> OUT, "wine "+cwd+"/bb16rc6win/BlackBox.exe -use \"`winepath -w \"$PWD\"`\" $* &"
 
 print >> OUT, "sleep 1"
 
-print >> OUT, "mv "+cwd+"/hide/Lin "+cwd+"/"
-print >> OUT, "mv "+cwd+"/hide/System "+cwd+"/"
-print >> OUT, "mv "+cwd+"/hide/Host "+cwd+"/"
-print >> OUT, "mv "+cwd+"/hide/Code "+cwd+"/"
+print >> OUT, "mv "+cwd+"/hide/* "+cwd+"/"
 print >> OUT, "rm "+cwd+"/hide -R"
 
 os.system('chmod +x "'+name+'"')
