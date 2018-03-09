164025fd7cdb276180154995a01dfe316165d097
Иван <d.ivan.krsk@gmail.com>
2018-03-10 00:58:52 +0700

    fix build of Windows version

--- BlackBox/_Windows_GUI/build.bat
+++ BlackBox/_Windows_GUI/build.bat
@@ -1,17 +1,5 @@
 @echo off
-@echo DevCompiler.CompileThis WinApi WinOle WinCtl WinCmc WinOleAut WinOleCtl WinDlg WinOleDlg WinMM WinSql WinRpc WinNet WinGL WinGLAux WinGLUtil Kernel Files HostFiles StdLoader Math SMath Log Strings Dates Meta Dialog Services Fonts Ports Printers Stores Converters Sequencers > tmp.txt
-@echo DevCompiler.CompileThis Models Views Controllers Properties Printing Mechanisms Containers Documents Windows StdCFrames Controls StdInterpreter StdDialog StdApi StdCmds HostRegistry HostFonts HostPorts OleData HostMechanisms HostWindows HostPrinters HostClipboard HostCFrames HostDialog HostCmds HostMenus HostPictures >> tmp.txt
-@echo DevCompiler.CompileThis TextModels TextRulers TextSetters TextViews TextControllers TextMappers StdLog TextCmds StdHeaders FormModels FormViews FormControllers FormGen FormCmds StdFolds StdLinks DevCommanders StdTables StdTabViews HostTabFrames StdViewSizer DevMarkers DevReferences DevDebug DevHeapSpy DevSearch DevSubTool DevAlienTool >> tmp.txt
-@echo DevCompiler.CompileThis HostPackedFiles DevPacker DevDependencies DevCmds DevInspector HostTextConv HostBitmaps HostMail StdMenuTool StdClocks StdStamps StdDebug StdCoder StdScrollers Out DevCPM DevCPT DevCPS DevCPB DevCPP DevCPE DevCPH DevCPL486 DevCPC486 DevCPV486 DevSelectors DevCompiler DevDecoder386 DevBrowser DevLinker DevRBrowser >> tmp.txt
-@echo DevCompiler.CompileThis OleStorage OleServer OleClient CtlT CtlC OleViews StdETHConv In XYplane Init Config Integers DevTypeLibs DevComInterfaceGen DevComDebug DevProfiler DevLinkChk DevMsgSpy DevAnalyzer CommStreams CommTCP CommV24 >> tmp.txt
-@echo DevCompiler.CompileThis SqlDrivers SqlOdbc SqlOdbc3 SqlDB SqlControls SqlBrowser SqlObxDB SqlObxUI SqlObxNets SqlObxViews SqlObxGen SqlObxExt SqlObxInit SqlObxTab >> tmp.txt
-@echo DevCompiler.CompileThis ComTools ComEnumRect ComObject ComConnect ComAggregate ComKoala ComKoalaTst ComKoalaExe ComKoalaDll ComEnum ComPhoneBook ComPhoneBookActiveX ComPhoneBookClient >> tmp.txt
-@echo DevCompiler.CompileThis XhtmlWriters XhtmlEntitySets XhtmlStdFileWriters XhtmlTextTableMarkers XhtmlExporter >> tmp.txt
-@echo DevCompiler.CompileThis CtlStdType CtlOffice CtlVBIDE CtlOfficeBinder CtlMSForms CtlExcel9 CtlWord9 CtlGraph9 CtlPowerPoint9 CtlOutlook9 CtlDAO36 CtlADODB CtlAccess9 >> tmp.txt
-@echo DevCompiler.CompileThis ObxActions ObxAddress0 ObxAddress1 ObxAddress2 ObxAscii ObxBitmap ObxBlackBox ObxButtons ObxCalc ObxCaps ObxControls ObxDialog ObxConv ObxCount0 ObxCount1 ObxCtrls ObxCubes ObxDB ObxExcel ObxFact ObxRandom ObxFern ObxFldCtrls ObxGraphs ObxHello0 ObxHello1 ObxLines ObxLinks ObxPhoneDB ObxLookup0 ObxLookup1 ObxMMerge ObxOmosi >> tmp.txt
-@echo DevCompiler.CompileThis ObxOpen0 ObxOpen1 ObxOrders ObxParCmd ObxPatterns ObxPDBRep0 ObxPDBRep1 ObxPDBRep2 ObxPDBRep3 ObxPDBRep4 ObxPhoneUI ObxPhoneUI1 ObxRatCalc ObxScroll ObxSample ObxTabs ObxTickers ObxTrap ObxTwins >> tmp.txt
-@echo DevCompiler.CompileThis ObxViews0 ObxViews1 ObxViews2 ObxViews3 ObxViews4 ObxViews5 ObxViews6 ObxViews10 ObxViews11 ObxViews12 ObxViews13 ObxViews14 ObxWrappers ObxContIter ObxPi ObxUnitConv ObxLabelLister ObxControlShifter ObxFileTree ObxTabViews ObxWordEdit ObxStores CommObxStreamsClient CommObxStreamsServer >> tmp.txt
-@echo DevCompiler.CompileThis Console HostConsole ConsLog ConsCompiler ConsInterp >> tmp.txt
+@echo DevCompiler.CompileSubs Com Comm Ctl Dev Form Host Obx Ole Sql Std System Text Win Xhtml Cons >> tmp.txt
 @echo DevLinker.Link BlackBox.exe := Kernel$+ Files HostFiles StdLoader 1 BlackBox.res 1 Applogo.ico 2 Doclogo.ico 3 SFLogo.ico 4 CFLogo.ico 5 DtyLogo.ico 6 folderimg.ico 7 openimg.ico 8 leafimg.ico 1 Move.cur 2 Copy.cur 3 Link.cur 4 Pick.cur 5 Stop.cur 6 Hand.cur 7 Table.cur >> tmp.txt
 @echo Kernel.Quit(0) >> tmp.txt
 dev0.exe < tmp.txt
--- BlackBox/_Windows_Interp/build.bat
+++ BlackBox/_Windows_Interp/build.bat
@@ -1,5 +1,5 @@
 @echo off
-@echo DevCompiler.CompileThis WinApi WinOle Kernel Files Console Math Strings Meta Log Dialog HostFiles HostRegistry HostLang HostConsole Stores Converters Dates Integers Sequencers Services SMath Fonts Ports Printers Models Views Printing Mechanisms Controllers Properties Containers Documents Windows TextModels TextMappers TextRulers TextSetters TextViews TextControllers TextCmds In StdDialog StdApi StdCFrames Controls StdCmds StdScrollers StdViewSizer StdClocks StdCoder StdFolds StdDebug StdStamps StdETHConv StdHeaders StdLinks StdLoader HostDialog StdLog Out ConsLog DevMarkers DevCommanders DevSelectors DevCPM DevCPT DevCPH DevCPB DevCPE DevCPS DevCPP DevCPL486 DevCPC486 DevCPV486 DevCompiler DevLinker HostFonts DevReferences DevDebug DevComDebug ConsCompiler StdInterpreter HostWindows HostDates HostTextConv ConsInterp Init Config ObxHello0 ObxPi ObxRandom ObxTrap > tmp.txt
+@echo DevCompiler.CompileSubs Win Sql System Std Host Cons Text Form Dev Xhtml Comm Obx > tmp.txt
 @echo DevLinker.Link dos BlackBoxInterp.exe := Kernel$+ Files HostFiles StdLoader >> tmp.txt
 @echo Kernel.Quit(0) >> tmp.txt
 dev0.exe < tmp.txt
