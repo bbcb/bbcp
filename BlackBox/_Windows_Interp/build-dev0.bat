ea0d86726887a46abd48c7841c704018a70f5040
TemirgaleevEE <http://oberoncore.ru/members/_>
2020-02-24 22:14:27 +0300

    020 - 1

--- BlackBox/_Windows_Interp/build-dev0.bat
+++ BlackBox/_Windows_Interp/build-dev0.bat
@@ -1,5 +1,5 @@
 @echo off
-@echo DevLinker.Link dos dev0new.exe := Kernel$+ Files HostFiles HostPackedFiles StdLoader >> tmp.txt
+@echo DevLinker.Link dos dev0new.exe := Kernel$+ Files HostEnv HostFiles HostPackedFiles StdLoader >> tmp.txt
 @echo Kernel.Quit(0) >> tmp.txt
 dev0.exe -BB_PACKED_FIRST < tmp.txt
 del tmp.txt
