b15b99342a0c9ffd3976b82933014b6e222c49a0
TemirgaleevEE <http://oberoncore.ru/members/_>
2020-02-24 21:18:26 +0300

    020 - 1

--- BlackBox/_Windows_Interp/build-dev0.bat
+++ BlackBox/_Windows_Interp/build-dev0.bat
@@ -1,6 +1,5 @@
 @echo off
-@echo DevCompiler.CompileThis HostDevPackedFiles > tmp.txt
-@echo DevLinker.Link dos dev0new.exe := Kernel$+ Files HostFiles HostDevPackedFiles StdLoader >> tmp.txt
+@echo DevLinker.Link dos dev0new.exe := Kernel$+ Files HostFiles HostPackedFiles StdLoader >> tmp.txt
 @echo Kernel.Quit(0) >> tmp.txt
-dev0.exe < tmp.txt
+dev0.exe -BB_PACKED_FIRST < tmp.txt
 del tmp.txt
--- BlackBox/_Windows_Interp/build.bat
+++ BlackBox/_Windows_Interp/build.bat
@@ -2,5 +2,5 @@
 @echo DevCompiler.CompileSubs Win Sql System Std Host Cons Text Form Dev Xhtml Comm Obx > tmp.txt
 @echo DevLinker.Link dos BlackBoxInterp.exe := Kernel$+ Files HostFiles StdLoader >> tmp.txt
 @echo Kernel.Quit(0) >> tmp.txt
-dev0.exe < tmp.txt
+dev0.exe -BB_PACKED_FIRST < tmp.txt
 del tmp.txt
