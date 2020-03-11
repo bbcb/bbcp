b482b203c24694460222073f8686d30e97ebd265
Ivan Denisov <d.ivan.krsk@gmail.com>
2020-03-11 07:02:23 +0700

    reduce using of Kernel and Strings for unicode conversions, introduce module Unicode, rename Utf8 to Utf

--- BlackBox/_Windows_Interp/build.bat
+++ BlackBox/_Windows_Interp/build.bat
@@ -1,6 +1,6 @@
 @echo off
 @echo DevCompiler.CompileSubs Win Sql System Std Host Cons Text Form Dev Xhtml Comm Obx > tmp.txt
-@echo DevLinker.Link dos BlackBoxInterp.exe := Kernel$+ Files HostEnv HostFiles StdLoader >> tmp.txt
+@echo DevLinker.Link dos BlackBoxInterp.exe := Kernel$+ Files Utf HostEnv HostFiles StdLoader >> tmp.txt
 @echo Kernel.Quit(0) >> tmp.txt
 dev0.exe -BB_PACKED_FIRST < tmp.txt
 del tmp.txt
