53d272584fc6b3678820006642a9eb09ce9fa061
Ivan Denisov <d.ivan.krsk@gmail.com>
2021-05-07 21:56:34 +0700

    change repository structure and add tiled interface (made by Anton Dmitriev)

---
+++ BlackBox/build-gui-windows.bat
@@ -0,0 +1,7 @@
+@echo off
+@echo DevCompiler.CompileThis WinApi WinOle KernelKernelWindows >> tmp.txt
+@echo DevCompiler.CompileSubs Com Comm Ctl Dev Form Ole Sql Std System Text Win Cons >> tmp.txt
+@echo DevLinker.Link BlackBox.exe := Unicode Kernel$+ Files Utf WinEnv WinFiles WinGui StdLoader WinLoader 1 BlackBox.res 1 Applogo.ico 2 Doclogo.ico 3 SFLogo.ico 4 CFLogo.ico 5 DtyLogo.ico 6 folderimg.ico 7 openimg.ico 8 leafimg.ico 1 Move.cur 2 Copy.cur 3 Link.cur 4 Pick.cur 5 Stop.cur 6 Hand.cur 7 Table.cur >> tmp.txt
+@echo Kernel.Quit(0) >> tmp.txt
+dev0win.exe /BB_PACKED_FIRST < tmp.txt
+del tmp.txt
