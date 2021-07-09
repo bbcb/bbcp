f608c5f60a4f896273fb54df06819d39e01014a6
Ivan Denisov <d.ivan.krsk@gmail.com>
2021-07-10 00:58:54 +0700

    merge new Kernel structure from Anton Dmitriev, rebuild console compilers for Linux and Windows, compiler always COM aware

--- BlackBox/Wincons/build-dev0.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@echo DevLinker.Link dos dev0new.exe := Kernel$+ Files Utf HostEnv HostFiles HostPackedFiles StdLoader >> tmp.txt
-@echo Kernel.Quit(0) >> tmp.txt
-dev0.exe -BB_PACKED_FIRST < tmp.txt
-del tmp.txt
--- BlackBox/Wincons/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-@echo DevCompiler.CompileSubs Win Sql System Std Host Cons Text Form Dev Xhtml Comm Obx > tmp.txt
-@echo DevLinker.Link dos BlackBoxInterp.exe := Kernel$+ Files Utf HostEnv HostFiles StdLoader >> tmp.txt
-@echo Kernel.Quit(0) >> tmp.txt
-dev0.exe -BB_PACKED_FIRST < tmp.txt
-del tmp.txt
