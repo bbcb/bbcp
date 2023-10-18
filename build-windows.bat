2f25cdde7653991b6725b6aaa8050373d59053fa
Ivan Denisov <d.ivan.krsk@gmail.com>
2023-10-18 23:34:02 +0700

    remove extra Kernel.Quit calls, rebuild dev0 files for all platforms

--- build-windows.bat
+++ build-windows.bat
@@ -1,5 +1,5 @@
 @echo off
-@echo DevCompiler.CompileSubs @Win Com Comm Ctl Dev Form Ole Sql Std System Text Win Cons Mdi Fig >> tmp.txt
+@echo DevCompiler.CompileSubs @Win Com Comm Ctl Dev Form Ole Sql Std System Text Win Cons Mdi Fig > tmp.txt
 @echo DevLinker.Link BlackBox.exe := Kernel$+ Utf WinKernel Files WinEnv WinFiles WinGui StdLoader WinLoader 1 BlackBox.res 1 Applogo.ico 2 Doclogo.ico 3 SFLogo.ico 4 CFLogo.ico 5 DtyLogo.ico 6 folderimg.ico 7 openimg.ico 8 leafimg.ico 1 Move.cur 2 Copy.cur 3 Link.cur 4 Pick.cur 5 Stop.cur 6 Hand.cur 7 Table.cur >> tmp.txt
 @echo DevLinker.Link BlackBoxMDI.exe := Kernel$+ Utf WinKernel Files WinEnv WinFiles WinGui StdLoader MdiLoader 1 BlackBox.res 1 Applogo.ico 2 Doclogo.ico 3 SFLogo.ico 4 CFLogo.ico 5 DtyLogo.ico 6 folderimg.ico 7 openimg.ico 8 leafimg.ico 1 Move.cur 2 Copy.cur 3 Link.cur 4 Pick.cur 5 Stop.cur 6 Hand.cur 7 Table.cur >> tmp.txt
 @echo DevLinker.Link BlackBoxInterp.exe := Kernel$+ Utf WinKernel Files WinEnv WinFiles StdLoader WinIntLoader >> tmp.txt
