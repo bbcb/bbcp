24965997df48a52066d2ea05f6b6ca893d0a4dab
Alexander Shiryaev <shiryaev.a.v@gmail.com>
2025-10-13 10:45:17 +0500

    FreeBSD 14.3

--- Fbsd/Xtras/Libc/sizeofs.c
+++ Fbsd/Xtras/Libc/sizeofs.c
@@ -1,8 +1,11 @@
 #include <sys/types.h>
 #include <sys/signal.h>
+#include <sys/resource.h>
 #include <setjmp.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 
 #define TABS "\t\t"
 
@@ -71,17 +74,18 @@ int main ()
 	D("blkcnt_t", sizeof(blkcnt_t), FALSE, FALSE);
 	D("blksize_t", sizeof(blksize_t), FALSE, FALSE);
 	D("int8_t", sizeof(int8_t), FALSE, TRUE);
-	D("uint8_t", sizeof(u_int8_t), FALSE, TRUE);
+	D("uint8_t", sizeof(uint8_t), FALSE, TRUE);
 	D("int16_t", sizeof(int16_t), FALSE, TRUE);
-	D("uint16_t", sizeof(u_int16_t), FALSE, TRUE);
+	D("uint16_t", sizeof(uint16_t), FALSE, TRUE);
 	D("int32_t", sizeof(int32_t), FALSE, TRUE);
-	D("uint32_t", sizeof(u_int32_t), FALSE, TRUE);
+	D("uint32_t", sizeof(uint32_t), FALSE, TRUE);
 	D("int64_t", sizeof(int64_t), FALSE, TRUE);
-	D("uint64_t", sizeof(u_int64_t), FALSE, TRUE);
+	D("uint64_t", sizeof(uint64_t), FALSE, TRUE);
 	D("wchar_t", sizeof(wchar_t), FALSE, TRUE);
+	D("rlim_t", sizeof(rlim_t), FALSE, TRUE);
 	D("sigjmp_buf", sizeof(sigjmp_buf), FALSE, TRUE);
 	D("intFlags", sizeof(int), TRUE, TRUE);
-	/* D("FILE", sizeof(FILE), FALSE, FALSE); */
+	D("FILE", sizeof(FILE), FALSE, FALSE);
 	D("fflags_t", sizeof(fflags_t), TRUE, TRUE);
 
 	printf("%ssigset_t* = ARRAY [untagged] %d OF BYTE;\n", TABS, (int)sizeof(sigset_t));
